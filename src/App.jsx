8073c540ac8490ec8f90d92f2f3e7baeaf06d9b4
Shreyan <shreyanbhandari36@gmail.com>
2024-12-30 17:58:35 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function Board() {
+  return <>
+    <div className="rows">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+    <div className="rows">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+    <div className="rows">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+  </>
+}
